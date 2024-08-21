4c4b8373fd20dab72d45e8073722f34e95d5dfae
yonasketema <yonask.kitaw@gmail.com>
2024-08-21 12:03:17 +0300

    fix :  prisma is not defined

--- app/(dashboard)/books/page.tsx
+++ app/(dashboard)/books/page.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Box } from "@mui/material";
+import prisma from "@/lib/prisma";
 import BookTable from "@/components/BookTable";
 import { getCurrentSignInUserServer } from "@/lib/authUser";
 import { redirect } from "next/navigation";
