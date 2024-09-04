8475f093c0f88575042551e0ab32bffc14f72970
yonasketema <yonask.kitaw@gmail.com>
2024-09-04 11:52:03 +0300

    feat: add query chain filtering
    feat: add query validation

--- lib/filterQuery.ts
+++ lib/filterQuery.ts
@@ -1,46 +1,90 @@
-import { BookStatus } from "@prisma/client";
+import jsonSchema from "@/data/json-schema.json";
+import { Prisma } from "@prisma/client";
 
-export const filterQuery = (req: Request, type?: string) => {
-  const { searchParams } = new URL(req.url);
+interface PrismaQuery {
+  [key: string]: string | number | PrismaQuery;
+}
 
-  const title = searchParams.get("title");
-  const minPrice = searchParams.get("minPrice");
-  const maxPrice = searchParams.get("maxPrice");
-  const author = searchParams.get("author");
-  const owner = searchParams.get("owner");
-  const category = searchParams.get("category");
-  const location = searchParams.get("location");
-  const status = searchParams.get("status") as BookStatus;
+export const filterQuery = (
+  req: Request,
+  model: Prisma.ModelName
+): PrismaQuery => {
+  const url = new URL(req.url);
+  const filters = Object.fromEntries(url.searchParams);
 
-  const filters: any = {};
+  if (!filters.filters) return {};
 
-  if (title) {
-    filters.title = { contains: title, mode: "insensitive" };
-  }
-  if (minPrice || maxPrice) {
-    filters.price = {};
-    if (Number(minPrice)) {
-      filters.price.gte = Number(minPrice);
+  console.log(JSON.parse(filters.filters));
+
+  let queryArray: { id: string; operVal: string; val: string }[] = JSON.parse(
+    filters.filters
+  );
+
+  const fieldExistsInSchema = (fieldPath: string, val: any): boolean => {
+    const keys: string[] = fieldPath.split(".");
+    let current: any = jsonSchema.definitions[model];
+
+    if (keys.length === 1 && keys[0] in current.properties) {
+      if (typeof val !== current.properties[keys[0]].type.toLocaleLowerCase()) {
+        throw new Error(
+          `Field ${keys[0]} type does not match the JSON schema.`
+        );
+      }
+      return true;
     }
-    if (Number(maxPrice)) {
-      filters.price.lte = Number(maxPrice);
+
+    for (const key of keys) {
+      if (key in current?.properties && keys.indexOf(key) === keys.length - 1) {
+        if (typeof val !== current.properties[key].type) {
+          throw new Error(`Field ${key} type does not match the JSON schema.`);
+        }
+        return true;
+      }
+
+      if (
+        key in current?.properties &&
+        jsonSchema.definitions[
+          current?.properties[key]["$ref"].split("/").at(-1) as Prisma.ModelName
+        ]
+      ) {
+        current =
+          jsonSchema.definitions[
+            current?.properties[key]["$ref"]
+              .split("/")
+              .at(-1) as Prisma.ModelName
+          ];
+      } else {
+        return false;
+      }
     }
-  }
-  if (status) {
-    filters.status = status;
-  }
 
-  // admin get books
+    return true;
+  };
 
-  if (author) filters.author = { contains: author, mode: "insensitive" };
+  const prismaQuery: PrismaQuery = queryArray.reduce(
+    (acc, { id, operVal, val }) => {
+      if (!fieldExistsInSchema(id, val)) {
+        throw new Error(`Field ${id} does not exist in the JSON schema.`);
+      }
 
-  if (owner && type !== "user")
-    filters.owner = { name: { contains: owner, mode: "insensitive" } };
-  if (owner && type === "user")
-    filters.name = { contains: owner, mode: "insensitive" };
-  if (category) filters.categoryId = category;
-  if (category) filters.categoryId = category;
-  if (location) filters.location = { contains: location, mode: "insensitive" };
+      const keys = id.split(".");
+      let current: PrismaQuery = acc;
 
-  return filters;
+      keys.forEach((key, index) => {
+        if (!current[key]) {
+          current[key] = {};
+        }
+        if (index === keys.length - 1) {
+          (current[key] as PrismaQuery)[operVal] = val;
+        } else {
+          current = current[key] as PrismaQuery;
+        }
+      });
+
+      return acc;
+    },
+    {}
+  );
+
+  return prismaQuery;
 };
